5eb0822ba5d4c4abce1cc157843e5c31f465e05a
Cato Wen <cato2394@gmail.com>
2023-07-12 17:27:34 -0400

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,26 @@
-import portrait from './assets/cato_portrait.png';
+import portrait from '../assets/cato_portrait.png';
 
 const Homepage = () => {
+    const [windowSize, setWindowSize] = useState({
+        width: window.innerWidth,
+        height: window.innerHeight
+      });
+    
+      useEffect(() => {
+        const handleResize = () => {
+          setWindowSize({
+            width: window.innerWidth,
+            height: window.innerHeight
+          });
+        };
+    
+        window.addEventListener('resize', handleResize);
+    
+        return () => {
+          window.removeEventListener('resize', handleResize);
+        };
+      }, []);
+    
     return (
         <img src={portrait}
              alt="Picture of Me"

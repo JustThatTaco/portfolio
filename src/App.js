f1f3de80f4541d9e40fbcf0969b24802a645c40e
Cato Wen <cato2394@gmail.com>
2023-07-15 16:05:30 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+
 import Homepage from './components/Home';
 import Navbar from './components/Navbar';
 import './App.css';
@@ -6,26 +6,6 @@ import './App.css';
 
 
 function App() {
-  const [windowSize, setWindowSize] = useState({
-    width: window.innerWidth,
-    height: window.innerHeight
-  });
-
-  useEffect(() => {
-    const handleResize = () => {
-      setWindowSize({
-        width: window.innerWidth,
-        height: window.innerHeight
-      });
-    };
-
-    window.addEventListener('resize', handleResize);
-
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
-
   return (
     <div className="App">
       <Navbar />

c718fe981ba5f681c0c9b7cf4c049619a81062e9
Cato Wen <cato2394@gmail.com>
2023-08-17 13:45:36 -0700

    added github logo

--- src/components/Footer2.js
+++ src/components/Footer2.js
@@ -5,22 +5,27 @@ import './Footer2.css';
 import wechat from '../assets/wechat_draw.png';
 import email from '../assets/email_draw.png';
 import ins from '../assets/ins_draw.png';
+import git from '../assets/github_draw.png';
 
 const Footer = () => {
     return (
       <div className="footer-container">
         <div className="footer-content">
           <div className="item">
-            <img className="foot_pic" src= {wechat} alt="Picture 1" />
+            <img className="foot_pic" src= {wechat} alt="Wechat Logo" />
             <p className="text"> catoywen </p>
           </div>
           <div className="item">
-            <img src= {email} className="foot_pic" alt="Picture 2" />
+            <img src= {email} className="foot_pic" alt="Gmail Logo" />
             <p className="text"> cyw6944@nyu.edu </p>
           </div>
           <div className="item">
-            <img src= {ins} className="foot_pic" alt="Picture 3" />
+            <img src= {ins} className="foot_pic" alt="Instagram Logo" />
             <p className="text"> cato.wen </p>
+          </div>
+          <div className="item">
+            <img src= {git} className="foot_pic" alt="Github Logo" />
+            <p className="text"> catoywen </p>
           </div>
         </div>
       </div>

558498b8949230ddf98d303ecbb19c451c978faf
Cato Wen <cato2394@gmail.com>
2023-08-16 17:27:44 -0700

    footer2 and readme update

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # This is my website
 
-This is my website, made using HTML, CSS, and Javascript under the react framework. It is hosted at this link https://catoywen.github.io/portfolio/ .
+This is my personal website, made using HTML, CSS, and Javascript under the react framework. It is hosted at this link https://catoywen.github.io/portfolio/.
+
+This is my first time coding a website. I didn't follow any tutorials and only
+used documentation and ChatGPT as reference. 
---
+++ src/components/Footer2.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import './Footer2.css';
+
+import wechat from '../assets/wechat.png';
+import email from '../assets/email.png';
+import ins from '../assets/ins.png';
+
+const Footer = () => {
+  return (
+    <div className="footer-container">
+      <div className="footer-content">
+        <div className="item">
+          <img src="wechat.png" alt="Wechat" />
+          <p> catoywen </p>
+        </div>
+        <div className="item">
+          <img src="email.png" alt="Email" />
+          <p> cyw6944@nyu.edu </p>
+        </div>
+        <div className="item">
+          <img src="ins.png" alt="Ins" />
+          <p>cato.wen</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;

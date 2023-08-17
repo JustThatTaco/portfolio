708a10b0926fd40996eb1da99d8ff63c3b2799c3
Cato Wen <cato2394@gmail.com>
2023-08-17 15:29:30 -0700

    working on resume page

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,26 +1,24 @@
 // import resume from '../assets/cato_resume_aug2023.pdf';
 
+import "./Resume.css";
+
 const Resume = () => {
     return (
-        <div>
-            <p> Test Test Test Test</p>
-            <p> Test Test Test Test</p>
-            <p> Test Test Test Test</p>
-            <p> Test Test Test Test</p>
-            <p> Test Test Test Test</p>
-            <p> Test Test Test Test</p>
-            <p> Test Test Test Test</p>
-        </div>
-        // <div>
-        // <h1> Resume </h1>
-        // <iframe
-        //     title="My Resume"
-        //     src = '../assets/cato_resume_aug2023.pdf'
-        //     width="90%"
-        //     // height="500px"
-        //     style={{ border: 'none' }}
-        // />
-        // </div>
+    <div className="resume_main">
+        <p className = "foreword"> The page is a little lackluster</p>
+        {/* <iframe
+            title="My Resume"
+            src = '../assets/cato_resume_aug2023.pdf'
+            width="90%"
+            // height="500px"
+            style={{ border: 'none' }}
+        /> */}
+        <object data="../assets/cato_resume_aug2023.pdf"
+                type="application/pdf" width="100%" height="100%">
+            {/* <p>Alternative text - include a link <a href="http://africau.edu/images/default/sample.pdf"> to the PDF!</a></p> */}
+            <p> my Resume </p>
+        </object>
+    </div>
     );
 };
   

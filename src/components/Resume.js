b16faaa0e8592aa1fdc3ec089dd925290ae9c5d1
Cato Wen <cato2394@gmail.com>
2023-08-18 13:00:35 -0700

    editing pdf rendering

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -8,8 +8,12 @@ const Resume = () => {
     return (
     <div className="resume_main">
         <p className = "foreword"> The page is a little lackluster</p>
-        <h4>Single Page</h4>
-        <Render pdf="/portfolio/resume.pdf" />
+  
+        <h4>All Pages</h4>
+        <div className="all-page-container">
+          <Render pdf="/portfolio/resume.pdf" />
+        </div>
+  
         <hr />
     </div>
     );

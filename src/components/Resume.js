f2114a9f8db41ffcb0c6052b5d1d5c408e03ba7f
Cato Wen <cato2394@gmail.com>
2023-08-18 10:47:52 -0700

    renamed resume

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -7,20 +7,8 @@ const Resume = () => {
     return (
     <div className="resume_main">
         <p className = "foreword"> The page is a little lackluster</p>
-        {/* <iframe
-            title="My Resume"
-            src = '../assets/cato_resume_aug2023.pdf'
-            width="90%"
-            // height="500px"
-            style={{ border: 'none' }}
-        /> */}
-        {/* <object data= "../assets/cato_resume_aug2023.pdf"
-                type="application/pdf" width="100%" height="100%">
-            <p>Alternative text - include a link <a href="http://africau.edu/images/default/sample.pdf"> to the PDF!</a></p>
-            <p> my Resume </p>
-        </object> */}
         <div style={{ width: '100%' }}>
-        <Document file= 'cato_resume_aug2023.pdf'>
+        <Document file= 'resume.pdf'>
           <Page pageNumber={1} width={window.innerWidth} />
         </Document>
         </div>

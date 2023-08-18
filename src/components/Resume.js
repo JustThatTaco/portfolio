bae0b3b76988638dd3547270bad784044221f9af
Cato Wen <cato2394@gmail.com>
2023-08-18 11:01:14 -0700

    added react pdf worker

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,15 +1,19 @@
 
 import React from 'react';
-import { Document, Page } from 'react-pdf';
+import { Document, Page, pdfjs } from 'react-pdf';
 import "./Resume.css";
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 const Resume = () => {
     return (
     <div className="resume_main">
         <p className = "foreword"> The page is a little lackluster</p>
         <div style={{ width: '100%' }}>
-        <Document file= 'resume.pdf'>
-          <Page pageNumber={1} width={window.innerWidth} />
+        <Document
+          file= "/resume.pdf"
+          onLoadError={console.error}
+        >
         </Document>
         </div>
     </div>

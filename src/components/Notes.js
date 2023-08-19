2587db1c30c3c71b2dcaedad9a01c93018337379
Cato Wen <cato2394@gmail.com>
2023-08-19 14:35:15 -0700

    updated notes page and uploaded some notes

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -11,12 +11,20 @@ const Notes = () => {
             took during college. These notes weren't originally intended to be
             read by others. I'll try to make my notes more organized in the future.  </p>
             <div className="wrapper_center">
+            <div>
+            <img src={sorry}
+                alt= "sorry uwu"
+                style = {{width: '20vw', height: 'auto'}}
+            />
+            </div>
             <a className="section_header"> Good </a> 
             <img src={good}
                 alt= "good"
                 className = 'image'
             />
-            </div><div className="wrapper_center">
+            </div>
+            <a className = "item" href={"/portfolio/catowen_calculus3_notes.pdf"} target="_blank"> MA-UY 2114 Calculus 3 Written Notes </a>
+            <div className="wrapper_center">
             <a className="section_header"> Usable </a>
             <img src={usable}
                 alt= "usable"
